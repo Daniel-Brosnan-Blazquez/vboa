3c8f2af429d4b625bf66738785f036098fa1454a
Daniel Brosnan <d.brosnan.b@gmail.com>
2019-10-25 19:01:38 +0200

    Add pagination to the BOA health view
    Extract pagination view to a common resource so that it is the same code for all the pages

--- src/vboa/boa_health/boa_health.py
+++ src/vboa/boa_health/boa_health.py
@@ -29,34 +29,34 @@ window_delay=0
 window_size=0.25
 repeat_cycle=5
 
-def get_start_stop_filters(window_size):
+def get_start_stop_filters(filters):
 
     start_filter = None
     stop_filter = None
     
     if request.method == "POST":
 
-        if request.form["start"] != "":
+        if filters["start"][0] != "":
             stop_filter = {
-                "date": request.form["start"],
+                "date": filters["start"][0],
                 "operator": ">="
             }
-            if request.form["stop"] == "":
+            if filters["stop"][0] == "":
                 start_filter = {
-                    "date": (parser.parse(request.form["start"]) + datetime.timedelta(days=window_size)).isoformat(),
+                    "date": (parser.parse(filters["start"][0]) + datetime.timedelta(days=window_size)).isoformat(),
                     "operator": "<="
                 }
             # end if            
         # end if
 
-        if request.form["stop"] != "":
+        if filters["stop"][0] != "":
             start_filter = {
-                "date": request.form["stop"],
+                "date": filters["stop"][0],
                 "operator": "<="
             }
-            if request.form["start"] == "":
+            if filters["start"][0] == "":
                 stop_filter = {
-                    "date": (parser.parse(request.form["stop"]) - datetime.timedelta(days=window_size)).isoformat(),
+                    "date": (parser.parse(filters["stop"][0]) - datetime.timedelta(days=window_size)).isoformat(),
                     "operator": ">="
                 }
             # end if
@@ -76,6 +76,13 @@ def show_health():
 
     template_name = request.args.get("template")
 
+    filters = {}
+    filters["limit"] = ["100"]    
+    if request.method == "POST":
+        filters = request.form.to_dict(flat=False).copy()
+    # end if
+    filters["offset"] = [""]
+
     # Initialize reporting period (now - window_size days, now)
     start_filter = {
         "date": (datetime.datetime.now() - datetime.timedelta(days=window_delay)).isoformat(),
@@ -85,7 +92,7 @@ def show_health():
         "date": (datetime.datetime.now() - datetime.timedelta(days=(window_delay+window_size))).isoformat(),
         "operator": ">="
     }
-    start_filter_calculated, stop_filter_calculated = get_start_stop_filters(window_size)
+    start_filter_calculated, stop_filter_calculated = get_start_stop_filters(filters)
 
     if start_filter_calculated != None:
         start_filter = start_filter_calculated
@@ -95,7 +102,24 @@ def show_health():
         stop_filter = stop_filter_calculated
     # end if
 
-    return query_health_and_render(start_filter, stop_filter, template_name = template_name)
+    filters["start"] = [stop_filter["date"]]
+    filters["stop"] = [start_filter["date"]]
+    filters["template_name"] = [template_name]    
+
+    return query_health_and_render(start_filter, stop_filter, template_name = template_name, filters = filters)
+
+@bp.route("/health-pages", methods=["POST"])
+def query_health_pages():
+    """
+    Health monitoring view of the BOA using pages.
+    """
+    current_app.logger.debug("Health monitoring view using pages")
+    filters = json.loads(request.form["json"])
+    start_filter, stop_filter = get_start_stop_filters(filters)
+
+    template_name = filters["template_name"][0]
+    
+    return query_health_and_render(start_filter, stop_filter, template_name = template_name, filters = filters)
 
 @bp.route("/sliding_health_parameters", methods=["GET", "POST"])
 def show_sliding_health_parameters():
@@ -184,7 +208,7 @@ def show_sliding_health():
 
     return query_health_and_render(start_filter, stop_filter, sliding_window, template_name = template_name)
 
-def query_health_and_render(start_filter = None, stop_filter = None, sliding_window = None, template_name = None):
+def query_health_and_render(start_filter = None, stop_filter = None, sliding_window = None, template_name = None, filters = None):
 
     kwargs = {}
 
@@ -199,7 +223,18 @@ def query_health_and_render(start_filter = None, stop_filter = None, sliding_win
     # end if
 
     kwargs["gauge_names"] = {"filter": ["BOA_HEALTH"], "op": "in"}
-    
+
+    # Set offset and limit for the query
+    if filters and "offset" in filters and filters["offset"][0] != "":
+        kwargs["offset"] = filters["offset"][0]
+    # end if
+    if filters and "limit" in filters and filters["limit"][0] != "":
+        kwargs["limit"] = filters["limit"][0]
+    # end if
+
+    # Set order by stop time descending
+    kwargs["order_by"] = {"field": "stop", "descending": True}
+
     health_events = query.get_events(**kwargs)
 
     reporting_start = stop_filter["date"]
@@ -210,4 +245,4 @@ def query_health_and_render(start_filter = None, stop_filter = None, sliding_win
         template = "boa_health/boa_health_" + template_name + ".html"
     # end if
     
-    return render_template(template, health_events=health_events, request=request, reporting_start=reporting_start, reporting_stop=reporting_stop, sliding_window=sliding_window)
+    return render_template(template, health_events=health_events, request=request, reporting_start=reporting_start, reporting_stop=reporting_stop, sliding_window=sliding_window, filters = filters)
