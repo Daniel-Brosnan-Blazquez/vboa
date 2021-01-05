9ec5397ceb4ef768c4b1df7c7b09e514759bff7d
JUBV <john.bradley@elecnor.es>
2021-01-05 14:25:35 +0100

    Modify draft test for the "general view of alerts" view

--- src/tests/test_general_view_alerts.py
+++ src/tests/test_general_view_alerts.py
@@ -72,39 +72,300 @@ class TestGeneralViewAlerts(unittest.TestCase):
     def test_sources_alerts_query_no_filter(self):
 
         # Insert data
-        data = {"operations":[{
-                "mode": "insert",
-                "dim_signature": {"name": "dim_signature",
-                                    "exec": "exec",
-                                    "version": "1.0"},
-                "source": {"name": "source.xml",
-                            "reception_time": "2018-07-05T02:07:03",
-                            "generation_time": "2018-07-05T02:07:03",
-                            "validity_start": "2018-06-05T02:07:03",
-                            "validity_stop": "2018-06-05T08:07:36",
-                            "priority": 10,
-                            "ingestion_completeness": {
-                                    "check": "false",
-                                    "message": "MISSING DEPENDENCIES"}
+        data = {"operations": [{
+            "mode": "insert",
+            "dim_signature": {"name": "dim_signature",
+                              "exec": "exec",
+                              "version": "1.0"},
+            "source": {"name": "source.json",
+                       "reception_time": "2018-06-06T13:33:29",
+                       "generation_time": "2018-07-05T02:07:03",
+                       "validity_start": "2018-06-05T02:07:03",
+                       "validity_stop": "2018-06-05T08:07:36"},
+            "alerts": [{
+                "message": "Alert message",
+                "generator": "test",
+                "notification_time": "2018-06-05T08:07:36",
+                "alert_cnf": {
+                    "name": "alert_name1",
+                    "severity": "critical",
+                    "description": "Alert description",
+                    "group": "alert_group"
                 },
-                "alerts": [{
-                    "message": "The input source.xml has been received to be ingested",
-                    "generator": "source.xml",
-                    "notification_time": "2018-06-05T04:07:03",
-                    "alert_cnf": {
-                        "name": "PENDING_INGESTION_OF_SOURCE",
-                        "severity": "fatal",
-                        "description": "Alert refers to the pending ingestion of the relative input",
-                        "group": "INGESTION_CONTROL"
-                    },
-                    "entity": {
-                        "reference_mode": "by_ref",
-                        "reference": "source.xml",
-                        "type": "source"
-                    }
-                }]
+                "entity": {
+                    "reference_mode": "by_ref",
+                    "reference": "source.json",
+                    "type": "source"
+                }
+            },{
+                "message": "Alert message",
+                "generator": "test1",
+                "notification_time": "2019-06-06T08:07:36",
+                "alert_cnf": {
+                    "name": "alert_name2",
+                    "severity": "critical",
+                    "description": "Alert description",
+                    "group": "alert_group"
+                },
+                "entity": {
+                    "reference_mode": "by_ref",
+                    "reference": "source.json",
+                    "type": "source"
+                }
             }]
+        }]
         }
+
+        # Check data is correctly inserted
+        self.engine_eboa.data = data
+        assert eboa_engine.exit_codes["OK"]["status"] == self.engine_eboa.treat_data()[0]["status"]
+        
+        wait = WebDriverWait(self.driver,5)
+
+        self.driver.get("http://localhost:5000/general_view_alerts/")
+
+        """ # Go to tab
+        functions.goToTab(self.driver,"Sources")
+        submit_button = wait.until(EC.visibility_of_element_located((By.ID,'sources-submit-button')))
+        functions.click_no_graphs_sources(self.driver)
+
+        # Click on query button
+        functions.click(submit_button) """
+
+    def test_events_alerts_query_no_filter(self):
+
+        # Insert data
+        data = {"operations": [{
+            "mode": "insert",
+            "dim_signature": {"name": "dim_signature",
+                              "exec": "exec",
+                              "version": "1.0"},
+            "source": {"name": "source.json",
+                       "reception_time": "2018-06-06T13:33:29",
+                       "generation_time": "2018-07-05T02:07:03",
+                       "validity_start": "2018-06-05T02:07:03",
+                       "validity_stop": "2019-06-05T08:07:36",
+                       "priority": 30},
+            "events": [{
+                "explicit_reference": "ER1",
+                "gauge": {"name": "GAUGE_NAME",
+                          "system": "GAUGE_SYSTEM",
+                          "insertion_type": "SIMPLE_UPDATE"},
+                "start": "2018-06-05T02:07:03",
+                "stop": "2018-06-05T08:07:36",
+                "values": [{"name": "VALUES",
+                            "type": "object",
+                            "values": [
+                                {"type": "text",
+                                 "name": "TEXT",
+                                 "value": "TEXT"},
+                                {"type": "boolean",
+                                 "name": "BOOLEAN",
+                                 "value": "true"}]
+                            }],
+                "alerts": [{
+                    "message": "Alert message",
+                    "generator": "test",
+                    "notification_time": "2018-06-05T08:07:36",
+                    "alert_cnf": {
+                        "name": "alert_name1",
+                        "severity": "critical",
+                        "description": "Alert description",
+                        "group": "alert_group"
+                    }},
+                           {
+                               "message": "Alert message",
+                               "generator": "test2",
+                               "notification_time": "2019-06-05T08:07:36",
+                               "alert_cnf": {
+                                   "name": "alert_name2",
+                                   "severity": "major",
+                                   "description": "Alert description",
+                                   "group": "alert_group2"
+                               }}]
+            },
+                       {
+                           "explicit_reference": "ER2",
+                           "gauge": {"name": "GAUGE_NAME2",
+                                     "system": "GAUGE_SYSTEM2",
+                                     "insertion_type": "SIMPLE_UPDATE"},
+                           "start": "2019-06-05T02:07:03",
+                           "stop": "2019-06-05T08:07:36",
+                           "alerts": [{
+                               "message": "Alert message",
+                               "generator": "test",
+                               "notification_time": "2018-06-05T08:07:36",
+                               "alert_cnf": {
+                                   "name": "alert_name2",
+                                   "severity": "critical",
+                                   "description": "Alert description",
+                                   "group": "alert_group"
+                               }},
+                                      {
+                                          "message": "Alert message",
+                                          "generator": "test2",
+                                          "notification_time": "2019-06-05T08:07:36",
+                                          "alert_cnf": {
+                                              "name": "alert_name3",
+                                              "severity": "major",
+                                              "description": "Alert description",
+                                              "group": "alert_group2"
+                                          }}]
+                       }]
+        }]
+                }
+
+        # Check data is correctly inserted
+        self.engine_eboa.data = data
+        assert eboa_engine.exit_codes["OK"]["status"] == self.engine_eboa.treat_data()[0]["status"]
+        
+        wait = WebDriverWait(self.driver,5)
+
+        self.driver.get("http://localhost:5000/general_view_alerts/")
+
+        """ # Go to tab
+        functions.goToTab(self.driver,"Sources")
+        submit_button = wait.until(EC.visibility_of_element_located((By.ID,'sources-submit-button')))
+        functions.click_no_graphs_sources(self.driver)
+
+        # Click on query button
+        functions.click(submit_button) """
+
+    def test_annotations_alerts_query_no_filter(self):
+
+        # Insert data
+        data = {"operations": [{
+            "mode": "insert",
+            "dim_signature": {"name": "dim_signature",
+                              "exec": "exec",
+                              "version": "1.0"},
+            "source": {"name": "source.json",
+                       "reception_time": "2018-06-06T13:33:29",
+                       "generation_time": "2018-07-05T02:07:03",
+                       "validity_start": "2018-06-05T02:07:03",
+                       "validity_stop": "2019-06-05T08:07:36",
+                       "priority": 30},
+            "annotations": [{
+                "explicit_reference": "ER1",
+                "annotation_cnf": {"name": "NAME",
+                                   "system": "SYSTEM",
+                                   "insertion_type": "SIMPLE_UPDATE"},
+                "values": [{"name": "VALUES",
+                            "type": "object",
+                            "values": [
+                                {"type": "text",
+                                 "name": "TEXT",
+                                 "value": "TEXT"},
+                                {"type": "boolean",
+                                 "name": "BOOLEAN",
+                                 "value": "true"}]
+                            }],
+                "alerts": [{
+                    "message": "Alert message",
+                    "generator": "test",
+                    "notification_time": "2018-06-05T08:07:36",
+                    "alert_cnf": {
+                        "name": "alert_name1",
+                        "severity": "critical",
+                        "description": "Alert description",
+                        "group": "alert_group"
+                    }},
+                           {
+                               "message": "Alert message",
+                               "generator": "test2",
+                               "notification_time": "2019-06-05T08:07:36",
+                               "alert_cnf": {
+                                   "name": "alert_name2",
+                                   "severity": "major",
+                                   "description": "Alert description",
+                                   "group": "alert_group2"
+                               }}]
+            },
+                       {
+                           "explicit_reference": "ER1",
+                           "annotation_cnf": {"name": "NAME",
+                                              "system": "SYSTEM",
+                                              "insertion_type": "SIMPLE_UPDATE"},
+                           "alerts": [{
+                               "message": "Alert message",
+                               "generator": "test",
+                               "notification_time": "2018-06-05T08:07:36",
+                               "alert_cnf": {
+                                   "name": "alert_name1",
+                                   "severity": "critical",
+                                   "description": "Alert description",
+                                   "group": "alert_group"
+                               }},
+                                      {
+                                          "message": "Alert message",
+                                          "generator": "test2",
+                                          "notification_time": "2019-06-05T08:07:36",
+                                          "alert_cnf": {
+                                              "name": "alert_name2",
+                                              "severity": "major",
+                                              "description": "Alert description",
+                                              "group": "alert_group2"
+                                          }}]
+                       }]
+        }]
+                }
+
+        # Check data is correctly inserted
+        self.engine_eboa.data = data
+        assert eboa_engine.exit_codes["OK"]["status"] == self.engine_eboa.treat_data()[0]["status"]
+        
+        wait = WebDriverWait(self.driver,5)
+
+        self.driver.get("http://localhost:5000/general_view_alerts/")
+
+        """ # Go to tab
+        functions.goToTab(self.driver,"Sources")
+        submit_button = wait.until(EC.visibility_of_element_located((By.ID,'sources-submit-button')))
+        functions.click_no_graphs_sources(self.driver)
+
+        # Click on query button
+        functions.click(submit_button) """
+
+    def test_explicit_refs_alerts_query_no_filter(self):
+
+        # Insert data
+        data = {"operations": [{
+            "mode": "insert",
+            "dim_signature": {"name": "dim_signature",
+                              "exec": "exec",
+                              "version": "1.0"},
+            "source": {"name": "source.json",
+                       "reception_time": "2018-06-06T13:33:29",
+                       "generation_time": "2018-07-05T02:07:03",
+                       "validity_start": "2018-06-05T02:07:03",
+                       "validity_stop": "2019-06-05T08:07:36",
+                       "priority": 30},
+            "explicit_references": [{
+                "name": "ER1",
+                "group": "ER_GROUP",
+                "alerts": [{
+                    "message": "Alert message",
+                    "generator": "test",
+                    "notification_time": "2020-06-05T08:07:36",
+                    "alert_cnf": {
+                        "name": "alert_name1",
+                        "severity": "critical",
+                        "description": "Alert description",
+                        "group": "alert_group"
+                    }},
+                           {
+                               "message": "Alert message",
+                               "generator": "test2",
+                               "notification_time": "2019-06-05T08:07:36",
+                               "alert_cnf": {
+                                   "name": "alert_name2",
+                                   "severity": "major",
+                                   "description": "Alert description",
+                                   "group": "alert_group2"
+                               }}]
+            }]
+        }]
+                }
 
         # Check data is correctly inserted
         self.engine_eboa.data = data
