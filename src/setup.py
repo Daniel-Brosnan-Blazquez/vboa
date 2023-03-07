d1287148da602f764decb7f7fa93e85dc7b98376
Daniel Brosnan Blázquez <daniel.brosnan@elecnor.es>
2023-03-07 16:26:49 +0100

    Update VBOA version

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ module vboa
 from setuptools import setup, find_packages
 
 setup(name="vboa",
-      version="1.0.1",
+      version="1.0.2",
       description="Visualization tool for Business Operation Analysis",
       url="https://bitbucket.org/dbrosnan/vboa/",
       author="Daniel Brosnan",
